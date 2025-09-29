e2aafb335e5bbec984c0a8af85e9cdbc170e7e56
eilseq <tools@eilseq.com>
2025-09-29 15:53:57 +0200

    Update Next Config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
+    const alias = {
+      "@": path.resolve(__dirname),
+      "@components": path.resolve(__dirname, "components"),
+      "@context": path.resolve(__dirname, "context"),
+      "@hooks": path.resolve(__dirname, "hooks"),
+      "@lib": path.resolve(__dirname, "lib"),
+    };
+
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "@": __dirname, // <-- This makes @/ point to project root
-      "@components": `${__dirname}/components`,
-      "@context": `${__dirname}/context`,
-      "@hooks": `${__dirname}/hooks`,
-      "@lib": `${__dirname}/lib`,
+      ...alias,
     };
     return config;
   },
